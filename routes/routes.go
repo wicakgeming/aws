5fb3540ddfdb1f9d0ecb2c1bc355e035058a528a
wicakgeming <DimasW17@gmail.com>
2025-05-30 02:28:40 +0700

    db3

--- routes/routes.go
+++ routes/routes.go
@@ -2,7 +2,7 @@ package routes
 
 import (
 	"github.com/gin-gonic/gin"
-	"github.com/wicakgeming/aws/controllers"
+	"github.com/wicakgeming/aws/controller"
 )
 
 func InitSensorRoutes(r *gin.Engine) {
